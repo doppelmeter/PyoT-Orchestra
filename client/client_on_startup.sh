165625278b5e6f6a7949a3cb3a240ac554f7c175
doppelmeter <huerzeler.marius@gmail.com>
2019-09-30 15:31:30 +0200

    Update client_on_startup.sh
    better GitHub support

--- client/client_on_startup.sh
+++ client/client_on_startup.sh
@@ -3,5 +3,9 @@
 # Save all shell and python scripts in ~/script/client
 # Add script as cronjob @reboot
 
-python3 ~/script/client/show_ip_on_sensehat.py
-python3 ~/script/client/show_sample_text_on_sensehat.py
+# Update from GitHub
+git pull https://github.com/doppelmeter/PyoT-Orchestra
+
+# Start Client Software
+python3 ~/PyoT-Orchestra/client/show_ip_on_sensehat.py
+python3 ~/PyoT-Orchestra/client/show_sample_text_on_sensehat.py
