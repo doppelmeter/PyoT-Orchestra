a16092255e23d82b40794d571d4ea94117ea4b99
Marius Huerzeler <huerzeler.marius@gmail.com>
2019-10-07 15:32:24 +0200

    add logging

--- client/musician_pc.py
+++ client/musician_pc.py
@@ -7,6 +7,8 @@ import time
 
 import keyboard
 import paho.mqtt.client as mqtt
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 from utils.display import triangel, piano, guitar
 from utils.functions import get_ip_adress
@@ -14,6 +16,7 @@ from utils.settings import *
 
 client = mqtt.Client()
 client.connect(settings.broker, settings.broker_port, 60)
+logging.info("Connected to "+settings.broker)
 
 client.loop_start()
 
@@ -45,7 +48,7 @@ while True:  # making a loop
             octave += 1
             if octave > 8:
                 octave = 0
-        print(str(scale[current_tones]))
+        logging.debug(str(scale[current_tones]))
     elif keyboard.is_pressed('down'):  # iy 'q' is pressed
         current_tones -= 1
         if current_tones < 0:
@@ -53,21 +56,21 @@ while True:  # making a loop
             octave -= 1
             if octave < 0:
                 octave = 8
-        print(str(scale[current_tones]))
+        logging.debug(str(scale[current_tones]))
     elif keyboard.is_pressed('right'):  # iy 'q' is pressed
         current_synt += 1
         if current_synt >= len(synt):
             current_synt = 0
-        print(current_synt)
+        logging.debug(current_synt)
     elif keyboard.is_pressed('left'):  # iy 'q' is pressed
         current_synt -= 1
         if current_synt < 0:
             current_synt = len(synt) - 1
-        print(current_synt)
+        logging.debug(current_synt)
     if keyboard.is_pressed('space'):  # if key 'space' is pressed
         send = f"{ip_adress};{scale[current_tones]}{octave};{current_synt}"
         client.publish(settings.topic_sound_msg, payload=send, qos=0, retain=False)
-        print("space ")
+        logging.debug(send)
     else:
         pass
     time.sleep(0.08)
